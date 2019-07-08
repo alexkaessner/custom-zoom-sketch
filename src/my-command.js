4896b63aa3412d3d4a2b3efb2cac8577e30ddbae
Alexander Käßner <alexkaessner@me.com>
2019-07-08 21:55:09 +0200

    added basic logic

--- src/my-command.js
+++ src/my-command.js
@@ -2,5 +2,31 @@ import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export default function() {
-  sketch.UI.message("It's alive 🙌")
+	var zoomLevel = 1.0
+
+	// ask user for input
+	sketch.UI.getInputFromUser(
+		"Zoom Level",
+		{
+			description: 'Set the zoom value in percent (without the % symbol).',
+			type: 'string',
+			initialValue: '100'
+		},
+		(err, value) => {
+			if (err) {
+				// most likely the user canceled the input
+	      return
+			} else {
+				zoomLevel = value
+			}
+		}
+	)
+
+	// check if user input is a valid number
+	if (zoomLevel == '' || zoomLevel < 1) {
+		sketch.UI.message('Please choose a valid zoom level.')
+	} else {
+		context.document.setZoomValue(zoomLevel/100)
+	}
+
 }
